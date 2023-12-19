ef43f748f58adf7be79dfb9c1b8f68f3d47876e4
Elvira <sabirova.elvira.gam@gmail.com>
2023-12-19 10:33:12 +0500

    feat: solve vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,30 +3,60 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+    this.code_abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+  encrypt(str, key) {
+    this.validate(str, key);
+    return this.crypt(str, key, true)
+
+  }
+  decrypt(str, key) {
+    this.validate(str, key);
+    return this.crypt(str, key, false)
+  }
+
+  crypt(str, key, mode) {
+    let shift = key.repeat(Math.ceil(str.length / key.length)).toUpperCase();
+    let upper_str = String(str).trim().toUpperCase();
+    let r = '';
+    for(let i = 0, j = 0; i < upper_str.length; i++) {
+      let code = upper_str.charCodeAt(i);
+      if(this.code_abc.indexOf(upper_str[i]) > -1) {
+        if(mode) code = code + shift[j++].charCodeAt(0);
+        else code = (code + this.code_abc.length - shift[j++].charCodeAt(0));
+        code = code % this.code_abc.length + 65;
+      }
+      r += String.fromCharCode(code);
+    }
+    return  this.isDirect ? r : r.split('').reverse().join('');
+  }
+
+  validate(str, key) {
+    if (!str || !key) {
+      throw new Error("Incorrect arguments!");
+    }
   }
 }
 
