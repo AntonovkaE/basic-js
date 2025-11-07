e88892f74a48d2c40c5ab0ff2a9de19ee6c426bf
Elvira <sabirova.elvira.gam@gmail.com>
2025-11-07 13:30:40 +0500

    feat: solve repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,11 +16,26 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function repeater(/* str, options */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options, ) {
+  let r = '';
+  const {repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|'} = options;
+  for (let i = 0; i < repeatTimes; i += 1) {
+    r += str;
+    for (let k = 0; k < additionRepeatTimes; k += 1) {
+      r += addition;
+      if (k !== additionRepeatTimes - 1) {
+        r += additionSeparator
+      }
+    }
+    if (i !== repeatTimes - 1) {
+      r += separator
+    }
+  }
+  return r;
 }
 
 module.exports = {
   repeater
 };
+
+
